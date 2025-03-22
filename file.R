eb1673be83e4c7266310ec94e32f85565828f1b8
fordeia <157367221+fordeia@users.noreply.github.com>
2025-03-22 16:37:01 -0400

    Update file.R

--- file.R
+++ file.R
@@ -1,7 +1,7 @@
 #Setting the directory
 setwd("C:/Users/fordeia/CherryBlossom")
 
-#CBloom <-read.table("cherryBlossomData.txt",header =TRUE,sep="\t", fill = TRUE)
+CBloom <-read.table("cherryBlossomData.txt",header =TRUE,sep="\t", fill = TRUE)
 CBloom4_24 <-read.table("Cherryblossom_2004-2024.txt",header =TRUE,sep="\t", fill = TRUE)
 
 #Dropping unwanted variables
