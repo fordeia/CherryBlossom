329bfd517ee5b2c154528f7bbd6e89c9861e5e66
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-01 22:03:20 -0400

    Update file.R

--- file.R
+++ file.R
@@ -8,5 +8,11 @@ CBloom4_24<-CBloom4_24[ , -c(1,3, 4, 5, 6,10,11,12,13,14,15,16)]
 
 #Dropping rows with NA
 CBloom4_24<-CBloom4_24[-c(22,23,24,25,26),]
-head(CBloom4_24)
+
+library(randomForest)
+set.seed(1237)
+CBloom4_24.rg <- randomForest(PEAK ~ ., data=CBloom4_24, importance=TRUE,
+                        proximity=TRUE)
+
+sqrt(sum((CBloom4_24.rg$predicted - CBloom4_24$PEAK)^2) / nrow(iris))
 
