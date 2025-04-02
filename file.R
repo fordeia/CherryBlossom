4b2136eceb35538806bc17a2824d5c79595bc919
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-01 20:54:33 -0400

    Update file.R

--- file.R
+++ file.R
@@ -1,11 +1,7 @@
-#Setting the directory
-#setwd("C:/Users/fordeia/CherryBlossom")
-
-#CBloom <-read.table("cherryBlossomData.txt",header =TRUE,sep="\t", fill = TRUE)
 CBloom4_24 <-read.table("Cherryblossom_2004-2024.txt",header =TRUE,sep="\t", fill = TRUE)
 
 #Dropping unwanted variables
-#CBloom<-CBloom[ , !(names(CBloom) %in% "X")]
+CBloom4_24<-CBloom4_24[ , !(names(CBloom4_24) %in% "YEAR")]
 
 # Create a new variable by summing two existing variables
     #CBloom$OceTemp <- 2*CBloom$El.Nino+CBloom$La.Nina
