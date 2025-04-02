ec0fa0ccd74ec44b9bf4ed07e0d65f2fcd48c6ce
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-01 20:41:05 -0400

    Update file.R

--- file.R
+++ file.R
@@ -14,28 +14,3 @@ CBloom4_24 <-read.table("Cherryblossom_2004-2024.txt",header =TRUE,sep="\t", fil
 CBloom4_24<-CBloom4_24[-c(22,23,24,25,26),]
 head(CBloom4_24)
 
-#Bootstrap sampling
-
-# Import library for bootstrap methods
-library(boot)
-# Import library for plotting
-library(ggplot2)
-diff.fun <- function(CBloom4_24, idx)
-{
-  df <- CBloom4_24[idx, ]
- 
-  # Find the mean difference for the weights 
-  # for men and women
-  c(mean(df[, 2])- mean(df[, 3]))
-}
-
-# Setting the seed for 
-# reproducability of results
-set.seed(25)
- 
-# Calling the boot function with the dataset
-# our function and no. of rounds
-bootstrap <- boot(CBloom4_24, diff.fun, R = 1000)
-
-# Display the result of boot function
-bootstrap
