62837961267d3f3cdf021f01e83bb2d827b2c9d0
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-01 21:55:05 -0400

    Update file.R

--- file.R
+++ file.R
@@ -1,11 +1,10 @@
 CBloom4_24 <-read.table("Cherryblossom_2004-2024.txt",header =TRUE,sep="\t", fill = TRUE)
 
-#Dropping unwanted variables
-CBloom4_24<-CBloom4_24[ , -c(1,3, 4, 5, 6,12,13,14,15,16)]
-
 # Create a new variable by summing two existing variables
 CBloom4_24$OceTemp <- 2*CBloom4_24$EL.NINO+CBloom4_24$LA.NINA
 
+#Dropping unwanted variables
+CBloom4_24<-CBloom4_24[ , -c(1,3, 4, 5, 6,10,11,12,13,14,15,16)]
 
 #Dropping rows with NA
 CBloom4_24<-CBloom4_24[-c(22,23,24,25,26),]
