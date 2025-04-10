89a47734801d120204be4d0ccb4beb5df5170521
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-09 22:36:25 -0400

    Update RFH20.R

--- RFH20.R
+++ RFH20.R
@@ -4,11 +4,12 @@ h2o.init()
 h2o.clusterInfo()
 
 #Loading packages
+install.packages("tidyverse")
 library(tidyverse)
 
 # read dataset, convert categorical variables to factor data type
-install.packages("readxl")
-install.packages("writexl")
+#install.packages("readxl")
+#install.packages("writexl")
 library(readxl)
 h_train <- read_excel("trainCB.xlsx")
 h_test <- read_excel("testCB.xlsx")
