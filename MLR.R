f039965e1059de6dd66345c702afc70a69f35697
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-12 12:36:56 -0400

    Update MLR.R

--- MLR.R
+++ MLR.R
@@ -3,6 +3,9 @@
 #install.packages("writexl")
 library(readxl)
 
+#Loading the datasets
+total <- read_excel("bootdataCBloom4_24.xlsx")
+
 h_train <- read_excel("h_train.xlsx")
 head(h_train)
 
@@ -10,8 +13,8 @@ h_test <- read_excel("h_test.xlsx")
 head(h_test)
 
 # Remove target variable
-temp_df <- subset(df, select = -c(Weight))
-melt_df <- melt(temp_df)
+temp_total <- subset(total, select = -c(PEAK))
+melt_total <- melt(temp_total)
 
 # Draw boxplot
-boxplot(data = melt_df, value ~ variable)
+boxplot(data = melt_total, value ~ variable)
