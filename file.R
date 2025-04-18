4e426e1ea2843556cfb46311f647b5cb0da2b0ee
fordeia <157367221+fordeia@users.noreply.github.com>
2025-04-18 17:30:15 -0400

    Update file.R

--- file.R
+++ file.R
@@ -33,7 +33,9 @@ pca_result <- prcomp(X, scale = FALSE)
 # return factor scores for first principal component
 x_result <- pca_result$x[,"PC1"]
 
-loadings <- pca_result$rotation. 
+#correlation between the pca and variables
+loadings <- pca_result$rotation
+loadings
 
 #Keep the first 2 components
 #pca_data <- as.data.frame(pca_result$x)[,1:2] # Select the first 2 PCs
